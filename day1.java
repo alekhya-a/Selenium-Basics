5858b4dbe5fbbf5e3d2c4ed35e74206d7c71158d
alekhya-a <50871580+alekhya-a@users.noreply.github.com>
2022-02-16 18:45:23 +0530

    TestNG 1st program

---
+++ day1.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day1 
+{
+
+	@Test  //annotation
+	public void Demo()  //method is compulsory
+	{	
+		System.out.println("hello"); //automation
+
+	}
+	
+	@Test  //annotation
+	public void SecondTest()  //method is compulsory
+	{	
+		System.out.println("bye"); //automation
+
+	}
+
+}
